7d3305b4b1c6f92a80e07a01adb679d2ced0d0d0
Morten S. Laursen <morten@softwarehuset.dk>
2012-11-15 17:12:33 +0100

    Added X11 attribute to allow opengl to work in a multithreaded environment

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    app.setAttribute(Qt::AA_X11InitThreads);
     cam_interface_test foo;
     foo.show();
     return app.exec();
